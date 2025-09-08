c535513b0668b61f1523cf65b6ca10839cdf8ddf
JonyChan2 <siri573644247siri@gmail.com>
2025-09-08 11:08:34 +0800

    modified order_process.py

--- order_process.py
+++ order_process.py
@@ -1,5 +1,15 @@
-import order_download as od
-
 
 
 #od.self()
+def format():
+    file = open('order.txt','r',encoding='utf-8')
+    lines = file.readlines()[1:-1]
+    orders = []
+    for line in lines:
+        cells = []
+        for cell in line.split('\t'):
+            if cell != '':
+                cells.append(cell)
+        orders.append(cells)
+    return orders
+
